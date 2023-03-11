8bd8fc5daa1d0ae3d800fd5b72497b2cf2f9a7ee
Ngo Tan Thieu <ngotanthieu369@gmail.com>
2023-03-11 21:31:03 +0700

    Update app.tsx after import all component

--- App.tsx
+++ App.tsx
@@ -6,15 +6,28 @@
  */
 
 import React from 'react';
-import {SafeAreaView, View} from 'react-native';
+import {SafeAreaView, ScrollView, StyleSheet, View} from 'react-native';
+import ContentJoke from './src/components/ContentJoke/ContentJoke';
+import Footer from './src/components/Footer/Footer';
 import Header from './src/components/Header/Header';
+import TitleJoke from './src/components/TitleJoke/TitleJoke';
 
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+  },
+});
 function App(): JSX.Element {
   return (
     <SafeAreaView>
-      <View>
-        <Header />
-      </View>
+      <ScrollView>
+        <View style={styles.container}>
+          <Header />
+          <TitleJoke />
+          <ContentJoke />
+          <Footer />
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 }
